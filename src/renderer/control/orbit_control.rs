7ae04b673f8cc4c116ac5c1975cb4851bb2df2ac
Ivor Wanders <ivor@iwanders.net>
2022-11-19 18:49:40 -0500

    OrbitControlConfig struct and constructor.
    To allow configuring the speeds of the OrbitControl.

--- src/renderer/control/orbit_control.rs
+++ src/renderer/control/orbit_control.rs
@@ -9,6 +9,41 @@ pub struct OrbitControl {
     new_target: Option<Vec3>,
 }
 
+/// Configuration struct to specify the (initial) properties of the OrbitControl.
+pub struct OrbitControlConfig {
+    /// The target to orbit around.
+    pub target: Vec3,
+    /// Minimum distance from target.
+    pub min_distance: f32,
+    /// Maximum distance from target.
+    pub max_distance: f32,
+    /// Speed of horizontal rotation around target.
+    pub speed_orbit_horizontal: f32,
+    /// Speed of vertical rotation around target.
+    pub speed_orbit_vertical: f32,
+    /// If set, speed of panning the target left-right of current view, disabled if None.
+    pub speed_orbit_target_left: Option<f32>,
+    /// If set, speed of panning the target top-bottom of current view, disabled if None.
+    pub speed_orbit_target_up: Option<f32>,
+    /// The speed at which to zoom towards the target.
+    pub speed_zoom: f32,
+}
+
+impl Default for OrbitControlConfig {
+    fn default() -> Self {
+        OrbitControlConfig {
+            target: Vec3::new(0.0, 0.0, 0.0),
+            min_distance: 1.0,
+            max_distance: 100.0,
+            speed_orbit_horizontal: 0.5,
+            speed_orbit_vertical: 0.5,
+            speed_orbit_target_left: None,
+            speed_orbit_target_up: None,
+            speed_zoom: 0.1,
+        }
+    }
+}
+
 impl OrbitControl {
     /// Creates a new orbit control with the given target and minimum and maximum distance to the target.
     pub fn new(target: Vec3, min_distance: f32, max_distance: f32) -> Self {
@@ -26,23 +61,37 @@ impl OrbitControl {
             new_target: Some(target),
         }
     }
-    /// Creates a new orbit control with the given target and minimum and maximum distance to the target,
-    /// the target can be moved with the right mouse button.
-    pub fn new_with_pan(target: Vec3, min_distance: f32, max_distance: f32) -> Self {
+
+    /// Create a new orbit configuration with the provided configuration.
+    pub fn new_with_config(config: OrbitControlConfig) -> Self {
+        let right_drag_horizontal = config
+            .speed_orbit_target_left
+            .map_or(CameraAction::None, |v| CameraAction::OrbitTargetLeft {
+                speed: v,
+            });
+        let right_drag_vertical = config
+            .speed_orbit_target_left
+            .map_or(CameraAction::None, |v| CameraAction::OrbitTargetUp {
+                speed: v,
+            });
         Self {
             control: CameraControl {
-                left_drag_horizontal: CameraAction::OrbitLeft { speed: 0.5 },
-                left_drag_vertical: CameraAction::OrbitUp { speed: 0.5 },
-                right_drag_horizontal: CameraAction::OrbitTargetLeft { speed: 0.5 },
-                right_drag_vertical: CameraAction::OrbitTargetUp { speed: 0.5 },
+                left_drag_horizontal: CameraAction::OrbitLeft {
+                    speed: config.speed_orbit_horizontal,
+                },
+                left_drag_vertical: CameraAction::OrbitUp {
+                    speed: config.speed_orbit_vertical,
+                },
+                right_drag_horizontal,
+                right_drag_vertical,
                 scroll_vertical: CameraAction::Zoom {
-                    min: min_distance,
-                    max: max_distance,
-                    speed: 0.1,
+                    min: config.min_distance,
+                    max: config.max_distance,
+                    speed: config.speed_zoom,
                 },
                 ..Default::default()
             },
-            new_target: Some(target),
+            new_target: Some(config.target),
         }
     }
 
